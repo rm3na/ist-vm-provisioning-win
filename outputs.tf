b0b6608197679ad5f91ab61defb0c18b4e264ce1
mtaylor0726 <mtaylor0726@gmail.com>
2021-07-20 17:30:22 -0500

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,4 @@
 
 output "vm_deploy" {
-  value = [vsphere_virtual_machine.vm_deploy.*.name, vsphere_virtual_machine.vm_deploy.*.default_ip_address]
+  value = [vsphere_virtual_machine.vm_deploy.*.name, vsphere_virtual_machine.vm_deploy.*.192.168.200.200]
 }
